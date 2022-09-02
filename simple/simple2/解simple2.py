ed148be6516637adab29ca240d7280ea522f84bc
春秋代序 <108599122+Durandal521@users.noreply.github.com>
2022-09-02 14:53:47 +0800

    Delete simple directory

--- simple/simple2/解simple2.py
+++
@@ -1,9 +0,0 @@
-v8 = ":\"AL_RT^L*.?+6/46"
-v7 = "harambe"
-print(v8)
-print(len(v8))
-print(v7)
-password = ''
-for i in range(len(v8)):
-    password += chr(ord(v7[i % 7]) ^ ord(v8[i]))
-print(password)
